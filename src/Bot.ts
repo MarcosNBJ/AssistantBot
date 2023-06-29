e12fc1474e9235c2dc165d29c5ccc4cf024ada24
MarcosN <marcosnery.comp@gmail.com>
2023-06-29 17:20:57 -0300

    feat: add linting

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,19 +2,19 @@ import { Client, GatewayIntentBits, Events } from 'discord.js';
 import config from './config';
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-	],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
 });
 
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on('interactionCreate', async interaction => {
+client.on('interactionCreate', async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === 'ping') {
@@ -26,4 +26,3 @@ client.on('interactionCreate', async interaction => {
 // });
 
 client.login(config.token);
-
