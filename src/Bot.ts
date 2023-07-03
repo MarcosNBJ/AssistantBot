389c2ba0d50c4610f3ae9074c4a48bba44c8a28f
MarcosN <marcosnery.comp@gmail.com>
2023-07-03 17:34:27 -0300

    refact: small improvement in variable names

--- src/Bot.ts
+++ src/Bot.ts
@@ -20,15 +20,15 @@ client.once(Events.ClientReady, (c) => {
 
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
-  const slashCommand = Commands.find((c) => c.name === interaction.commandName);
-  if (!slashCommand) {
+  const command = Commands.find((c) => c.name === interaction.commandName);
+  if (!command) {
     interaction.followUp({ content: 'An error has occurred' });
     return;
   }
 
   await interaction.deferReply();
 
-  slashCommand.run(interaction);
+  command.run(interaction);
 });
 
 client.on('messageCreate', async (message) => {
@@ -36,17 +36,17 @@ client.on('messageCreate', async (message) => {
   if (!message.content.startsWith(config.COMMAND_PREFIX)) return;
 
   const args = message.content.slice(config.COMMAND_PREFIX.length).trim().split(/ +/g);
-  const command = args.shift()?.toLowerCase();
+  const commandName = args.shift()?.toLowerCase();
 
-  if (!command) return;
+  if (!commandName) return;
 
-  const slashCommand = Commands.find((c) => c.name === command);
-  if (!slashCommand) {
+  const command = Commands.find((c) => c.name === commandName);
+  if (!command) {
     message.reply('An error has occurred');
     return;
   }
 
-  slashCommand.run(message);
+  command.run(message);
 });
 
 client.login(config.TOKEN);
