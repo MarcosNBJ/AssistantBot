f5698c5469ceeb456d510936aeeea5791694e8a3
MarcosN <marcosnery.comp@gmail.com>
2023-07-05 03:27:27 -0300

    feat: service to schedule recurring reminder

---
+++ src/services/ScheduleRecurringReminderService.ts
@@ -0,0 +1,26 @@
+import { Inject, Service } from 'typedi';
+import { JobQueueService } from '../JobQueueService';
+
+@Service()
+export default class ScheduleRecurringReminderService {
+  @Inject()
+  private jobqueue!: JobQueueService;
+
+  async execute(
+    reminderData: {
+      content: string,
+      channelId: string,
+      pattern: string,
+    },
+  ) {
+    const id = Math.random().toString(36).substring(7);
+    await this.jobqueue.queue.add(`reminder-${id}`, {
+      content: reminderData.content,
+      channelId: reminderData.channelId,
+    }, {
+      repeat: {
+        pattern: reminderData.pattern,
+      },
+    });
+  }
+}
